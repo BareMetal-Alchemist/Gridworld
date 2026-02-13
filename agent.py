3219294d4f7b7bfab9dc5922cdfe8017078d8147
BareMetal-Alchemist <nscott16@student.rccd.edu>
2026-02-13 14:34:45 -0800

    Finished BFS

--- agent.py
+++ agent.py
@@ -1,32 +1,63 @@
 # CPSC 481
 # Noah Scott
 
+from collections import deque
+
+
+
 # Gridworld Map
-# G = goal, . = empty, x = wall
+# S = start, G = goal, . = empty, x = wall
 Gridworld = [
-    [".", ".", ".", "."],
-    [".", ".", ".", "."],
-    [".", ".", ".", "."],
-    [".", ".", ".", "G"]
+    ["S", "x", "x", "."],
+    [".", ".", "x", "."],
+    ["x", ".", ".", "."],
+    ["x", ".", "x", "G"]
 ]
 
-#Grid config
-starting_x = 0
-starting_y = 0
-
-# Agent Config
-agent_posX = 
-agent_posY = 
+# Config
+starting_state = (0, 0)
+goal_state = (3, 3)
+agent_state = starting_state
 agent_avatar = "A"
+neighbors = deque()
+neighbors.append(agent_state) 
+visited = set()
+parent = {}
 
+while len(neighbors) > 0:
+    vertex = neighbors.popleft()
 
-# Movement is limited to Up, Down, Left and Right
-# Because of this we would adjust the agent's position 
-# -1 or +1 in the x or y axis
-# We are looking for breadth aka scanning the adjacent neighbors
-# This limitation in movement is helpful when considering BFS
+    if not vertex in visited:
+        visited.add(vertex)
+        # direction = [up, down, left, right]
+        directions = [(vertex[0] - 1, vertex[1]), (vertex[0] + 1, vertex[1]), (vertex[0], vertex[1] - 1), (vertex[0], vertex[1] + 1)]
+        
+        for dir in directions:
+            if (dir[0] >= 0 and dir[0] < len(Gridworld)) and (dir[1] >= 0 and dir[1] < len(Gridworld[dir[0]])) and not dir in visited and Gridworld[dir[0]][dir[1]] != "x":
+                neighbors.append(dir)
+                parent[dir] = vertex
+#    print(vertex)
+path = []
+path.append(goal_state)
+path_pos = goal_state
 
+while path_pos != starting_state:
+    path.append(parent[path_pos])
+    path_pos = parent[path_pos]
+final_path = list(reversed(path))
 
-# Gameplay loop
-while agent_position != "G":
-    
+for row in range (0, len(Gridworld)):
+    for col in range (0, len(Gridworld[row])):
+        print(Gridworld[row][col], " ", end="")
+    print()
+print()
+
+curr = 0
+
+for i in range (0, len(Gridworld)):
+    for j in range (0, len(Gridworld[i])):
+        if (i, j) == final_path[curr]:
+            Gridworld[i][j] = str(curr)
+            curr +=1
+        print(Gridworld[i][j]," ",  end="")
+    print()
